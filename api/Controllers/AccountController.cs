feb57db37e7b90b383623d55736950013a8bc285
beau <a02278030@aggies.usu.edu>
2025-05-29 15:02:13 -0600

    adding login api call

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using api.Models;
 using api.Dtos.Account;
 using api.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
@@ -16,10 +17,37 @@ namespace api.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly ITokenService _tokenService;
-        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
+        private readonly SignInManager<AppUser> _signInManager;
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
             _tokenService = tokenService;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _userManager.Users.FirstOrDefaultAsync(u => u.UserName == loginDto.Username);
+
+            if (user == null)
+            {
+                return Unauthorized("Invalid username");
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (!result.Succeeded)
+            {
+                return Unauthorized("Username not found and/or password is incorrect");
+            }
+            
+            return Ok(_tokenService.CreateToken(user));
         }
 
         [HttpPost("register")]
